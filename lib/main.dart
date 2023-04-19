7f2b30d68dc9421d872545a0de1519e2d9a41e1d
admin <admin@example.com>
2023-04-19 07:14:43 +0100

    changes for splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:edu_weather_app/view_models/weather.dart';
 import 'package:edu_weather_app/widgets/weather_info.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: WidgetsBinding.instance);
   runApp(const Application());
+
+
+  // FlutterNativeSplash.remove();
+
 }
 
 class Application extends StatelessWidget {
